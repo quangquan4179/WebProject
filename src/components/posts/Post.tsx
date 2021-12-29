d6ef19849d0c00b397ad4ac53f51edeb397d2778
Duy Linh <doretienshinh2@gmail.com>
2021-12-29 15:46:37 +0700

    css comment

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles, withStyles, WithStyles , } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles, withStyles, WithStyles, } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import CloseIcon from '@material-ui/icons/Close';
 import Card from '@material-ui/core/Card';
@@ -16,7 +16,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Box} from '@material-ui/core'
+import { Box, Modal, Fade } from '@material-ui/core'
 import { Nullable } from '../../shared/interfaces';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import Dialog from '@material-ui/core/Dialog';
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     media: {
       height: 0,
-      paddingTop: '56.25%', 
+      paddingTop: '56.25%',
     },
     expand: {
       transform: 'rotate(0deg)',
@@ -45,32 +45,107 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     avatar: {
       backgroundColor: red[500],
+      marginRight: '5px',
     },
-    dialog:{
-      display: 'flex'
+    dialog: {
+      display: 'flex',
     },
-    mydiv:{
+    mydiv: {
       display: 'flex',
       position: 'fixed',
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)'
+    },
+    countLikes: {
+      padding: '0 16px',
+      color: 'black',
+      fontWeight: 700,
+    },
+    postFooter: {
+      zIndex: 2,
+      backgroundColor: '#fff',
+      width: 'auto',
+      height: '40px',
+      bottom: '0px',
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: '16px',
+      justifyContent: 'space-between',
+      borderTop: '1px solid rgba(var(--b6a,219,219,219),1)',
+      padding: '11px 16px',
+    },
+    commentInput: {
+      width: '100%',
+      border: '0',
+      '&:focus': {
+        outline: '0'
+      }
+    },
+    commentButton: {
+      cursor: 'pointer',
+      marginRight: '5px',
+      color: 'rgba(var(--d69,0,149,246),1)'
+    },
+    postOwnerName: {
+      fontWeight: 700,
+      paddingRight: '5px',
+      color: 'black',
+    },
+    viewAllComments: {
+      padding: '0 16px 16px 16px',
+      cursor: 'pointer',
+    },
+    headerComment: {
+      height: '32px',
+      position: 'relative',
+      top: 0,
+      display: 'flex',
+      backgroundColor: '#fff',
+      width: 'auto',
+      padding: '14px 16px',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      borderBottom: '1px solid rgba(var(--b6a,219,219,219),1)',
+    },
+    bodyComment: {
+      height: 'calc(100% - 124px)',
+      position: "relative",
+    },
+    footerComment: {
+      zIndex: 2,
+      backgroundColor: '#fff',
+      width: 'auto',
+      height: '40px',
+      bottom: '0px',
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: '16px',
+      justifyContent: 'space-between',
+      borderTop: '1px solid rgba(var(--b6a,219,219,219),1)',
+      padding: '11px 16px',
+    },
+    commentItem: {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      padding: '14px 16px'
     }
   }),
 );
 const styles = (theme: Theme) =>
-createStyles({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-})
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  })
 
 
 export interface DialogTitleProps extends WithStyles<typeof styles> {
@@ -125,7 +200,7 @@ export default function Post(props: Props) {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-  const handleClickOpen = () => {
+  const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
@@ -133,79 +208,100 @@ export default function Post(props: Props) {
   };
   return (
     <Box sx={{ width: '70%' }} m={1} >
-        <Card >
-            <CardHeader
-                avatar={
-                <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                </Avatar>
-                }
-                action={
-                <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                </IconButton>
-                }
-                title={props.data.title}
-                subheader="September 14, 2016"
-            />
-            <CardMedia
-                className={classes.media}
-                image={props.data.photoURL}
-                title="Paella dish"
-            />
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {props.data.content}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-                <IconButton onClick={handleClickOpen}>
-                  < ChatBubbleOutlineIcon/>
-                </IconButton>
-            </CardActions>
-            
-        </Card>
-        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} fullScreen>
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-        </DialogTitle>
-        <DialogContent >
-        <Box   sx={{
-          bgcolor:'#484C4E',
-          height:'100vh',
+      <Card >
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={props.data.title}
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image={props.data.photoURL}
+          title="Paella dish"
+        />
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          {/* <IconButton >
+            < ChatBubbleOutlineIcon />
+          </IconButton> */}
+        </CardActions>
+        <div className={classes.countLikes}>77,413 likes</div>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <span className={classes.postOwnerName}>Tên thằng post</span>
+            {props.data.content}
+          </Typography>
+        </CardContent>
+        <div className={classes.viewAllComments} onClick={handleOpen}>View all comments</div>
 
+        <div className={classes.postFooter}>
+          <input placeholder="Add a comment . . ." className={classes.commentInput} />
+          <div className={classes.commentButton}>Post</div>
+        </div>
+      </Card>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        style={{ display: 'flex' }}
+      >
+        <div style={{
+          width: '70%', height: '90vh', display: 'flex',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)', backgroundColor: '#fff'
         }}>
-          <Box className={classes.mydiv} >
-
-          <Box width={'800px'} height={'100%'} >
-                  <Card >
-              
-                      <CardMedia
-                          className={classes.media}
-                          image={props.data.photoURL}
-                          title="Paella dish"
-                      />
-                      
-                  </Card>
-                </Box>
-                <Box  width={'700px'} sx={{
-                  bgcolor:'red'
-                }}>
-                COMMENT HERE
-                  
-                </Box>
-          </Box>
-        </Box>
-        </DialogContent>
-        
-      </Dialog>
+          <div style={{ width: '65%' }}>
+            <img src={props.data.photoURL} alt="" style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
+          </div>
+          <div style={{ width: '35%' }}>
+            <div className={classes.headerComment}>
+              <div>
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              </div>
+              <div>Tên</div>
+            </div>
+            <div className={classes.bodyComment}>
+              <div className={classes.commentItem}>
+                <div>
+                  <Avatar aria-label="recipe" className={classes.avatar} style={{ width: '30px', height: '30px', fontSize: '16px' }}>
+                    R
+                  </Avatar>
+                </div>
+                <div>
+                  <span style={{fontWeight: 500, color: 'black', marginRight: '5px'}}>Tên</span>
+                  <span>
+                    comment asdasdasdassssssssssssssss as dddddddddddddd dasdassssssssssssssss as dddddddddddddd dasdassssssssssssssss as dddddddddddddd
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className={classes.footerComment}>
+              <input placeholder="Add a comment . . ." className={classes.commentInput} />
+              <div className={classes.commentButton}>Post</div>
+            </div>
+          </div>
+        </div>
+      </Modal>
 
     </Box>
-    
+
   );
 }
